5eeadf7a41faea0a98dc2453d6da07f903bc0590
Delon Newman <delon.newman@gmail.com>
2011-12-09 14:23:41 -1000

    should be ready for initial release

---
+++ lib/sinatra/rest-service-auth.rb
@@ -0,0 +1,31 @@
+require 'digest'
+require 'yaml'
+require 'sinatra/base'
+
+module Sinatra
+	module RESTServiceAuth
+  	def base_url
+  		request.url.gsub(/\&sig\=.*/, '')
+  	end
+  
+  	def authorized?
+  		key, sig = params[:key], params[:sig]
+  		key == get_key && sig == sha256(base_url)
+  	end
+  
+  	def block!
+  		response['WWW-Authenticate'] = %(Basic realm="Restricted Area")
+  		throw(:halt, [401, "Not authorized\n"])
+  	end
+  
+  	def get_key
+  		@key ||= ((config = YAML.load_file(File.expand_path(File.join(File.dirname(__FILE__), 'config.yml')))) && config['key'])
+  	end
+  
+  	def sha256(str)
+  		Digest::SHA2.new(256).hexdigest(str)
+  	end
+	end
+
+	helpers RESTServiceAuth
+end
