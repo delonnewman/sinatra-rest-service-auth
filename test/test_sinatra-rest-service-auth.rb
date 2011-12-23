bf0881210d89c5ebd4fbad16e1e38a7bce4f4d29
Delon Newman <delon.newman@gmail.com>
2011-12-23 11:48:16 -1000

    removed tabs

--- test/test_sinatra-rest-service-auth.rb
+++ test/test_sinatra-rest-service-auth.rb
@@ -8,50 +8,50 @@ require 'digest'
 ENV['RACK_ENV'] = 'test'
 
 class TestApp < Sinatra::Base
-	helpers Sinatra::RESTServiceAuth
+  helpers Sinatra::RESTServiceAuth
 
-	helpers do
-		def match_key(key)
-			'34' == key
-		end
-	end
+  helpers do
+    def match_key(key)
+      '34' == key
+    end
+  end
 
-	before do
-		block unless authorized?
-	end
+  before do
+    block unless authorized?
+  end
 
-	get '/' do
-		'ok'
-	end
+  get '/' do
+    'ok'
+  end
 end
 
 class TestSinatraRestServiceAuth < Test::Unit::TestCase
-	include Rack::Test::Methods
+  include Rack::Test::Methods
 
-	def app
-		TestApp.new
-	end
+  def app
+    TestApp.new
+  end
 
-	def test_no_auth
-		get '/'
-		assert !last_response.ok?, "request should fail"
-	end
+  def test_no_auth
+    get '/'
+    assert !last_response.ok?, "request should fail"
+  end
 
-	def test_auth_for_short_query
-		q   = '?key=34'
-		url = "http://example.org/#{q}"
-		sig = Digest::SHA2.new(256).hexdigest(url)
-		get "/#{q}&sig=#{sig}"
-		assert last_response.ok?, "request should succeed"
-	end
+  def test_auth_for_short_query
+    q   = '?key=34'
+    url = "http://example.org/#{q}"
+    sig = Digest::SHA2.new(256).hexdigest(url)
+    get "/#{q}&sig=#{sig}"
+    assert last_response.ok?, "request should succeed"
+  end
 
-	def test_auth_for_long_query
-		# fails because cannot guarentee param order
-		params = [%w{key 34}, %w{name John}, %w{age 32}, %w{city Honolulu}]
-		q   = "?#{params.sort { |a, b| a.first <=> b.first }.map { |x| "#{x.first}=#{x.last}" }.join('&')}"
-		url = "http://example.org/#{q}"
-		sig = Digest::SHA2.new(256).hexdigest(url)
-		get "/#{q}&sig=#{sig}"
-		assert last_response.ok?, "request should succeed"
-	end
+  def test_auth_for_long_query
+    # fails because cannot guarentee param order
+    params = [%w{key 34}, %w{name John}, %w{age 32}, %w{city Honolulu}]
+    q   = "?#{params.sort { |a, b| a.first <=> b.first }.map { |x| "#{x.first}=#{x.last}" }.join('&')}"
+    url = "http://example.org/#{q}"
+    sig = Digest::SHA2.new(256).hexdigest(url)
+    get "/#{q}&sig=#{sig}"
+    assert last_response.ok?, "request should succeed"
+  end
 end
